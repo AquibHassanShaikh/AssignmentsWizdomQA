9e91933319a23bdb56ced51f7f3ff1ae52699bd6
AquibHassan <47009123+AquibHassan@users.noreply.github.com>
2020-11-20 14:22:03 +0000

    Class 2

---
+++ src/assignmentsclass2/Assignemnt7.java
@@ -0,0 +1,18 @@
+package assignmentsclass2;
+
+//Check whether Java is installed on your computer.	
+public class Assignemnt7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		verifyJavaVerison();
+
+	}
+
+	public static void verifyJavaVerison() {
+
+		System.out.println("Java Version :" + System.getProperty("java.version"));
+
+	}
+}
---
+++ src/assignmentsclass2/Assignment8.java
@@ -0,0 +1,22 @@
+package assignmentsclass2;
+
+///Compare two numbers	
+public class Assignment8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		compare(10, 11);
+	}
+
+	public static void compare(int num1, int num2) {
+
+		if (num1 == num2)
+
+			System.out.println(num1 + " == " + num2);
+		if (num1 != num2)
+
+			System.out.println(num1 + " != " + num2);
+	}
+
+}
